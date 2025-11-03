d1b0b98508b43a194b9143dd8f6face09319d1b8
Felipe <felipedd@gmail.com>
2025-11-03 14:32:56 -0300

    limpio estructura de archivos

--- select_stratgy.py
+++
@@ -1,6 +0,0 @@
-# select_strategy.py
-from config import SYMBOL, TRADING_MODE
-from strategy_selector import select_best_strategy
-
-symbol_to_use = "BTC/USDT:USDT" if TRADING_MODE == "futures" else SYMBOL
-select_best_strategy(symbol=symbol_to_use, days=30)
